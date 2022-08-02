bab312765a69114e5d62c7c01744008973917da7
Mohammad Pesaraklou <mhmdpsr6@gmail.com>
2022-08-02 15:09:00 +0430

    finish and build a simple quote app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+//styles
+import './App.css';
+
+
 
 
 function App() {
+
+  const [quote , setQuote] = useState([])
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+     const req = await axios.get('https://api.adviceslip.com/advice')
+     const res = await req.data.slip.advice
+     setQuote(res)
+
+}
+
+
+  
+
   return (
-    <div className="App">
-     
+    <div className="container">
+      <div className="card">
+        <h3 className="heading">{quote}</h3>
+        <button className="btn" onClick={() => getData()}>Give me a Quote</button>
+      </div>
     </div>
   );
 }
